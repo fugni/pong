e5a44077672fadb3ef939daf0a57123739fcd1b2
fungi <sylvienuijtemans@gmail.com>
2023-03-29 22:19:15 +0200

    it looks fancy

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+const scoreboardA = document.getElementById("scoreboard-a");
+const scoreboardB = document.getElementById("scoreboard-b");
+
+let scoreA = 0;
+let scoreB = 0;
+
+scoreboardA.innerText = scoreA;
+scoreboardB.innerText = scoreB;
