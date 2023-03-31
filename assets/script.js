5e708c77b3abf9b6e05f571c0fa585a1193aab8b
fungi <sylvienuijtemans@gmail.com>
2023-03-31 11:45:40 +0200

    it totally works and isnt randomly teleporting

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,7 @@
+import {KeyHandler} from "./input.js";
+
+const keys = new KeyHandler();
+
 const scoreboard1 = document.getElementById("scoreboard1");
 const scoreboard2 = document.getElementById("scoreboard2");
 
@@ -7,64 +11,54 @@ let score2 = 0;
 const paddle1 = document.getElementById("paddle1");
 const paddle2 = document.getElementById("paddle2");
 
-const paddleVelocity = 16;
+const paddleVelocity = 4;
 let paddle1Pos = 200;
 let paddle2Pos = 200;
 
-function paddleUp(paddleX, paddleXPos) {
-    paddle1Pos = paddle1Pos - paddleVelocity;
-    paddle1.style.top = paddle1Pos + "px";
+const ball = document.getElementById("ball");
+let ballPosX = 242;
+let ballPosY = 392;
+
+function gameLoop() {
+    if (keys.getKey("KeyW")) {
+        if (paddle1Pos > 0) {
+            paddle1Pos = paddle1Pos - paddleVelocity;
+            paddle1.style.top = paddle1Pos + "px";
+        }
+    }
+    if (keys.getKey("KeyS")) {
+        if (paddle1Pos < 368) {
+            paddle1Pos = paddle1Pos + paddleVelocity;
+            paddle1.style.top = paddle1Pos + "px";
+        }
+    }
+    if (keys.getKey("KeyI")) {
+        if (paddle2Pos > 0) {
+            paddle2Pos = paddle2Pos - paddleVelocity;
+            paddle2.style.top = paddle2Pos + "px";
+        }
+    }
+    if (keys.getKey("KeyK")) {
+        if (paddle2Pos < 368) {
+            paddle2Pos = paddle2Pos + paddleVelocity;
+            paddle2.style.top = paddle2Pos + "px";
+        }
+    }
+
+    ballPosX = Math.random() * 500;
+    ballPosY = Math.random() * 800;
+
+    ball.style.top = Math.round(ballPosX) + "px";
+    ball.style.left = Math.round(ballPosY) + "px";
+
+    scoreboard1.innerText = score1;
+    scoreboard2.innerText = score2;
+    
+    score1 += Math.round(Math.random());
+    score2 += Math.round(Math.random());
+
+    requestAnimationFrame(gameLoop);
+    
 }
 
-// const controller = {
-//     KeyW: {pressed: false, func: paddleUp(paddle1, paddle1Pos)},
-//     KeyS: {pressed: false, func: paddleDown(paddle1, paddle1Pos)},
-//     KeyI: {pressed: false, func: paddleUp(paddle2, paddle2Pos)},
-//     KeyK: {pressed: false, func: paddleDown(paddle2, paddle2Pos)},
-// }
-
-window.addEventListener("keydown", function(e) {
-    switch (e.code) {
-        case "KeyW":
-            if (paddle1Pos <= 0) {
-
-            } else {
-                paddleUp(paddle1, paddle1Pos)
-            }
-            break;
-        case "KeyS":
-            if (paddle1Pos >= 370) {
-                
-            } else {
-                paddle1Pos = paddle1Pos + paddleVelocity;
-                paddle1.style.top = paddle1Pos + "px";
-            }
-            break;
-    }
-})
-
-window.addEventListener("keydown", function(e) {
-    switch (e.code) {
-        case "KeyI":
-            if (paddle2Pos <= 0) {
-        
-            } else {
-                paddle2Pos = paddle2Pos - paddleVelocity;
-                paddle2.style.top = paddle2Pos + "px";
-            }
-            break;
-        case "KeyK":
-            if (paddle2Pos >= 370) {
-                
-            } else {
-                paddle2Pos = paddle2Pos + paddleVelocity;
-                paddle2.style.top = paddle2Pos + "px";
-            }
-            break;
-    }
-})
-
-
-
-scoreboard1.innerText = score1;
-scoreboard2.innerText = score2;
+gameLoop();
